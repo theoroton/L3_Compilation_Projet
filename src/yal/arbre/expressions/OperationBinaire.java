7eb30b60b24eebc80d33e222c40cc38ad0ca8c28
Théo Roton <theo.roton5@etu.univ-lorraine.fr>
2020-02-09 14:27:21 +0100

    fix ecrire

--- src/yal/arbre/expressions/OperationBinaire.java
+++ src/yal/arbre/expressions/OperationBinaire.java
@@ -10,6 +10,7 @@ public class OperationBinaire extends Expression {
 
     public OperationBinaire(Expression e1, Operation op, Expression e2, int n) {
         super(n);
+
         expGauche = e1;
         expDroite = e2;
         operation = op;
@@ -25,27 +26,41 @@ public class OperationBinaire extends Expression {
     @Override
     public String toMIPS() {
         StringBuffer mips = new StringBuffer();
-        if (expGauche.getClass() == IDF.class){
-            mips.append(expGauche.toMIPS());
-        } else if (expGauche.getClass().getSuperclass() == Constante.class){
-            mips.append(expGauche.toMIPS());
-        } else {
-            mips.append(expGauche.toMIPS());
-        }
 
-        mips.append("\tsw $v0, ($sp)\n");
-        mips.append("\tadd $sp, $sp, -4\n\n");
+//        if (expGauche.getClass() == IDF.class){
+//            mips.append(expGauche.toMIPS());
+//        } else if (expGauche.getClass().getSuperclass() == Constante.class){
+//            mips.append(expGauche.toMIPS());
+//        } else {
+//            mips.append(expGauche.toMIPS());
+//        }
+//
+//        mips.append("\tsw $v0, ($sp)\n");
+//        mips.append("\tadd $sp, $sp, -4\n\n");
+//
+//        if (expDroite.getClass() == IDF.class){
+//            mips.append(expDroite.toMIPS());
+//        } else if (expDroite.getClass().getSuperclass() == Constante.class){
+//            mips.append(expDroite.toMIPS());
+//        } else {
+//            mips.append(expDroite.toMIPS() + "\n");
+//        }
+//
+//        mips.append("\tadd $sp, $sp, 4\n");
+//        mips.append("\tlw $t8,($sp)\n\n");
 
-        if (expDroite.getClass() == IDF.class){
+        mips.append(expGauche.toMIPS());
+
+        if (expDroite.getClass().getSuperclass() == Constante.class){
+            mips.append("\tmove $t8, $v0\n");
             mips.append(expDroite.toMIPS());
-        } else if (expDroite.getClass().getSuperclass() == Constante.class){
-            mips.append(expDroite.toMIPS());
         } else {
-            mips.append(expDroite.toMIPS() + "\n");
+            mips.append("\tsw $v0, ($sp)\n");
+            mips.append("\taddi $sp, $sp, -4\n\n");
+            mips.append(expDroite.toMIPS());
+            mips.append("\taddi $sp, $sp, 4\n");
+            mips.append("\tlw $t8, ($sp)\n\n");
         }
-
-        mips.append("\tadd $sp, $sp, 4\n");
-        mips.append("\tlw $t8,($sp)\n\n");
 
         mips.append("\t" + operation.toMIPS() + "\n");
 
